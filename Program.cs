6797dd2a0deaaf2796500e2703d43d85eb501610
Mini <mini@minizbot.org>
2025-02-28 11:11:51 -0500

    Missing pex handling and Dotnet 8

--- Program.cs
+++ Program.cs
@@ -11,72 +11,84 @@ internal class Program
     {
         var settings = new JsonSerializerSettings();
         settings.AddMutagenConverters();
-        foreach (var pchFile in Directory.GetFiles("SynPKGs/PapyrusPatcher"))
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        foreach (var pchFile in Directory.EnumerateFiles(Path.Join("SynPKGs", "PapyrusPatcher")))
         {
-            var conf = JsonConvert.DeserializeObject<Config>(File.ReadAllText(pchFile), settings);
+            Console.WriteLine($"Running {pchFile}");
+            var conf = JsonConvert.DeserializeObject<Config>(File.ReadAllText($"{pchFile}"), settings);
             if (conf == null) return;
             foreach (var file in conf.patches)
             {
-                var pexed = PexFile.CreateFromFile($"Scripts/{file.FileName}", Mutagen.Bethesda.GameCategory.Skyrim);
-                if (pexed.MachineName == "PAPPATCHED")
+                var scriptName = Path.Join("Scripts", file.FileName);
+                if (File.Exists(scriptName))
                 {
-                    Console.WriteLine($"Papyrus file patched already, skipping {file.FileName}");
-                    continue;
-                }
-                pexed.MachineName = "PAPPATCHED";
-                foreach (var state in file.states)
-                {
-                    var obj = pexed.Objects.Where(x => x.Name == state.Obj).First();
-                    var st = obj.States.Where(x => x.Name == state.State).First();
-                    foreach (var patch in state.funcPatch)
+                    var pexed = PexFile.CreateFromFile(scriptName, Mutagen.Bethesda.GameCategory.Skyrim);
+                    if (pexed.MachineName == "PAPPATCHED")
                     {
-                        var fn = st.Functions.Where(x => x.FunctionName == patch.FunctionName).First().Function;
-                        if (patch.temps != null)
+                        Console.WriteLine($"Papyrus file patched already, skipping {file.FileName}");
+                        continue;
+                    }
+                    pexed.MachineName = "PAPPATCHED";
+                    foreach (var state in file.states)
+                    {
+                        var obj = pexed.Objects.Where(x => x.Name == state.Obj).First();
+                        var st = obj.States.Where(x => x.Name == state.State).First();
+                        foreach (var patch in state.funcPatch)
                         {
-                            fn.Locals.AddRange(patch.temps.GetData());
-                        }
-                        if (patch.insert != null)
-                        {
-                            foreach (var ins in patch.insert)
+                            var fn = st.Functions.Where(x => x.FunctionName == patch.FunctionName).First().Function;
+                            if (patch.temps != null)
                             {
-                                var pre = fn.Instructions.FindIndex(ins.pred.IsInst);
-                                foreach (var inst in ins.instructions)
+                                fn.Locals.AddRange(patch.temps.GetData());
+                            }
+                            if (patch.insert != null)
+                            {
+                                foreach (var ins in patch.insert)
                                 {
-                                    fn.Instructions.Insert(pre, new PexObjectFunctionInstruction()
+                                    var pre = fn.Instructions.FindIndex(ins.pred.IsInst);
+                                    foreach (var inst in ins.instructions)
                                     {
-                                        Arguments = inst.args.GetData(),
-                                        OpCode = inst.opCode
-                                    });
-                                    pre++;
+                                        fn.Instructions.Insert(pre, new PexObjectFunctionInstruction()
+                                        {
+                                            Arguments = inst.args.GetData(),
+                                            OpCode = inst.opCode
+                                        });
+                                        pre++;
+                                    }
                                 }
                             }
-                        }
-                        if (patch.rewrite != null)
-                        {
-                            foreach (var rw in patch.rewrite)
+                            if (patch.rewrite != null)
                             {
-                                var pre = fn.Instructions.FindIndex(rw.pred.IsInst);
-                                var instruction = fn.Instructions[pre];
-                                instruction.Arguments.Clear();
-                                instruction.OpCode = rw.newInst;
-                                instruction.Arguments.AddRange(rw.args.GetData());
-                            }
-                        }
-                        if (patch.rwArgs != null)
-                        {
-                            foreach (var rw in patch.rwArgs)
-                            {
-                                var pre = fn.Instructions.FindIndex(rw.pred.IsInst);
-                                var instruction = fn.Instructions[pre];
-                                foreach (var arg in rw.args)
+                                foreach (var rw in patch.rewrite)
                                 {
-                                    instruction.Arguments[arg.index] = arg.dat.GetData();
+                                    var pre = fn.Instructions.FindIndex(rw.pred.IsInst);
+                                    var instruction = fn.Instructions[pre];
+                                    instruction.Arguments.Clear();
+                                    instruction.OpCode = rw.newInst;
+                                    instruction.Arguments.AddRange(rw.args.GetData());
+                                }
+                            }
+                            if (patch.rwArgs != null)
+                            {
+                                foreach (var rw in patch.rwArgs)
+                                {
+                                    var pre = fn.Instructions.FindIndex(rw.pred.IsInst);
+                                    var instruction = fn.Instructions[pre];
+                                    foreach (var arg in rw.args)
+                                    {
+                                        instruction.Arguments[arg.index] = arg.dat.GetData();
+                                    }
                                 }
                             }
                         }
                     }
+                    pexed.WritePexFile(scriptName, Mutagen.Bethesda.GameCategory.Skyrim);
                 }
-                pexed.WritePexFile($"Scripts/{file.FileName}", Mutagen.Bethesda.GameCategory.Skyrim);
+                else
+                {
+                    Console.WriteLine($"Missing {scriptName}");
+                }
+                Console.WriteLine("Press any key to exit");
+                Console.In.Read();
             }
         }
     }
